0441d298f0c1d36a694b1f4fc996ebb8b0011458
LAPTOP_PCUESTA\pablo <pcuestamorer@gmail.com>
2023-11-24 20:39:03 -0500

    end chapter 5

--- 06/06_03/06_03b/main.py
+++ 06/06_03/06_03b/main.py
@@ -1,1 +1,14 @@
 from collections import deque
+
+history_stack = deque()
+history_stack.append("https://google.com")
+history_stack.append("https://linkedin.com")
+history_stack.append("https://stackoverflow.com")
+
+# View the top without remove it
+top = history_stack[-1]
+print(top)
+# Pop the top
+top = history_stack.pop()
+print(top)
+
